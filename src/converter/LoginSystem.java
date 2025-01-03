b68e40a7ba88261fc7b6a7a132c610a8f8ce3869
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-01-03 00:11:36 -0300

    first commit

---
+++ src/converter/LoginSystem.java
@@ -0,0 +1,28 @@
+package converter;
+
+import java.util.Scanner;
+
+public class LoginSystem {
+	   public static void main(String[] args) {
+		   
+	      Scanner scanner = new Scanner(System.in);
+	      
+	      
+	      String username = "admin";
+	      String password = "password";
+
+	      System.out.print("Enter username: ");
+	      String inputUsername = scanner.nextLine();
+
+	      System.out.print("Enter password: ");
+	      String inputPassword = scanner.nextLine();
+
+	      if(inputUsername.equals(username) && inputPassword.equals(password)) {
+	         System.out.println("Successful login");
+	      } else {
+	         System.out.println("Incorrect username or password");
+	      }
+
+	      scanner.close();
+	   }
+	}
